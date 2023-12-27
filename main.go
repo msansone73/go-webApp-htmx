474e1d4ebab696391d79b68e64a6b6a9dc139201
msansone73 <msansone@gmail.com>
2023-12-27 13:00:19 +0000

    #16 Added log settings (#17)

--- main.go
+++ main.go
@@ -1,16 +1,25 @@
 package main
 
 import (
+	"log"
 	"main/rotes"
+	"os"
 
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
 )
 
+var file *os.File
+
 func main() {
 
 	store := cookie.NewStore([]byte("secret"))
+
+    // Configurar o Gin para usar o logger padrão
+    gin.SetMode(gin.ReleaseMode)
+    gin.DefaultWriter = file
+
 
 	r := gin.Default()
 	r.Use(sessions.Sessions("mysession", store))
@@ -20,10 +29,16 @@ func main() {
 	r.Static("/static", "./assets")
 	
 	r.Run(":80")
-
-
 }
 
+func init() {
+    log.SetFlags(log.LstdFlags | log.Lshortfile)
+    file, err := os.OpenFile("app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+    if err != nil {
+        log.Fatal("Falha ao abrir arquivo de log:", err)
+    }
+    log.SetOutput(file)
+}
 
 
 
