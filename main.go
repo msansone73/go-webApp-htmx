913abab92488ed3d079cdc5cc61809b8332494bd
marcio sansone <msansone@gmail.com>
2024-01-12 13:18:39 +0000

    Add io package and set log parameter

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"io"
 	"log"
 	"main/rotes"
 	"os"
@@ -15,20 +16,17 @@ import (
 )
 
 
+func init() {
+	setLog()
+}
+
 func main() {
-
-
 	store := cookie.NewStore([]byte("secret"))
 	store.Options(sessions.Options{
         MaxAge: 3600 * 1, // 1 hours
         Path:   "/",
         Secure: true,
     })
-    // Configurar o Gin para usar o logger padrão
-    //gin.SetMode(gin.ReleaseMode)
-    //gin.DefaultWriter = file
-
-
 	r := gin.Default()
 	r.SetFuncMap(template.FuncMap{
 		"formatCurrency": formatCurrency,
@@ -43,7 +41,13 @@ func main() {
 	r.Run(":80")
 }
 
-func init() {
+
+
+// Set log parameter
+func setLog(){
+	f, _ := os.Create("app.log")
+	gin.DefaultWriter = io.MultiWriter(f)
+
     log.SetFlags(log.LstdFlags | log.Lshortfile)
     file, err := os.OpenFile("app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
     if err != nil {
@@ -53,25 +57,14 @@ func init() {
 }
 
 func formatCurrency(input float64) string {
-    // Format the number as a string with 2 decimal places
     s := fmt.Sprintf("%.2f", input)
-
-    // Split the string into the integer part and the decimal part
     parts := strings.Split(s, ".")
-
-    // Add commas as thousand separators to the integer part
     for i := len(parts[0]) - 3; i > 0; i -= 3 {
         parts[0] = parts[0][:i] + "." + parts[0][i:]
     }
-
-    // Join the parts back together
     return "$" + parts[0] + "," + parts[1]
 }
 
-
-// create a function that format a date like dd/mm/yyyy
 func formatDate(t time.Time) string {
-
-
 	return t.Format("02/01/2006")
 }
